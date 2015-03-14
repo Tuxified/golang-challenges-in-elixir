57aaaa09bfcd5bcd72b3a5f206d96b8098a6d76b
Tonći Galić <tuxified@gmail.com>
2015-03-14 02:42:54 +0100

    Decode sheet/body as well, almost done

--- challenge-1-drum_machine/lib/decoder.ex
+++ challenge-1-drum_machine/lib/decoder.ex
@@ -1,24 +1,50 @@
-defmodule Drum.Decoder do
+defmodule Drum.Track do
+  defstruct name: "", id: 0, pattern: []
+end
 
+defimpl String.Chars, for: Drum.Track do
+  def to_string(track), do: "(#{track.id}) #{track.name}\t|#{pattern_to_string(track.pattern)}|"
+
+  defp pattern_to_string(pattern) do
+    pattern
+    |> Enum.map(&(if &1 == 0, do: "-", else: "x"))
+    |> Enum.chunk(4)
+    |> Enum.join "|"
+  end
+end
+
+defmodule Drum.Decoder do
   def decode_file(path) do
     binary_content = File.read! path
     << "SPLICE",
-      raw_size    :: integer-size(64)-big,
+      _sheet_size :: integer-size(64)-big,
       raw_version :: binary-size(32),
       raw_tempo   :: float-size(32)-little,
       raw_sheet   :: binary >> = binary_content
 
-    version = raw_version |> stringify_version
-    sheet = raw_sheet |> decode_sheet
-    tempo = raw_tempo |> stringify_tempo
-    "Saved with HW Version: #{version}
-Tempo: #{tempo}
-"
+    version = stringify_version raw_version
+    tracks =  decode_sheet raw_sheet, []
+    tempo =  stringify_tempo raw_tempo
+
+    """
+    Saved with HW Version: #{version}
+    Tempo: #{tempo}
+    """ <> Enum.map_join(tracks, "\n", &String.Chars.to_string/1) <> "\n"
   end
 
-  defp decode_sheet(<< track_id :: integer-size(8), word_size :: integer-size(32), track :: binary >>) do
+  defp decode_sheet(<< track_id :: integer-size(8), word_size :: integer-size(32), track :: binary >>, []) do
     << track_name :: binary-size(word_size), track_pattern :: binary-size(16), rest :: binary >> = track
+    new_track = %Drum.Track{ id: track_id, name: track_name, pattern: to_char_list(track_pattern) }
+    decode_sheet(rest, [new_track])
   end
+
+  defp decode_sheet(<< track_id :: integer-size(8), word_size :: integer-size(32), track :: binary >>, tracks) do
+    << track_name :: binary-size(word_size), track_pattern :: binary-size(16), rest :: binary >> = track
+    new_track = %Drum.Track{ id: track_id, name: track_name, pattern: to_char_list(track_pattern) }
+    decode_sheet(rest, [new_track | tracks])
+  end
+
+  defp decode_sheet("", tracks), do: Enum.reverse tracks
 
   defp stringify_version(raw_version), do: raw_version |> to_char_list |> Enum.reject &(&1 == 0)
   defp stringify_tempo(raw_tempo), do: raw_tempo |> Float.round(1)
--- challenge-1-drum_machine/test/decoder_test.exs
+++ challenge-1-drum_machine/test/decoder_test.exs
@@ -4,21 +4,59 @@ defmodule DecoderTest do
 
   test "decode pattern1" do
     patterns = [
-    {"pattern_1.splice", "Saved with HW Version: 0.808-alpha
-Tempo: 120.0
-"},
-    {"pattern_2.splice",
-       "Saved with HW Version: 0.808-alpha
-Tempo: 98.4
-"},
-    {"pattern_3.splice",
-       "Saved with HW Version: 0.808-alpha
-Tempo: 118.0
-"}, {"pattern_4.splice",
-       "Saved with HW Version: 0.909
-Tempo: 240.0
-"}
-  ]
+      {"pattern_1.splice",
+        """
+        Saved with HW Version: 0.808-alpha
+        Tempo: 120.0
+        (0) kick	|x---|x---|x---|x---|
+        (1) snare	|----|x---|----|x---|
+        (2) clap	|----|x-x-|----|----|
+        (3) hh-open	|--x-|--x-|x-x-|--x-|
+        (4) hh-close	|x---|x---|----|x--x|
+        (5) cowbell	|----|----|--x-|----|
+        """,
+      },
+      {"pattern_2.splice",
+        """
+        Saved with HW Version: 0.808-alpha
+        Tempo: 98.4
+        (0) kick	|x---|----|x---|----|
+        (1) snare	|----|x---|----|x---|
+        (3) hh-open	|--x-|--x-|x-x-|--x-|
+        (5) cowbell	|----|----|x---|----|
+        """,
+      },
+      {"pattern_3.splice",
+        """
+        Saved with HW Version: 0.808-alpha
+        Tempo: 118.0
+        (40) kick	|x---|----|x---|----|
+        (1) clap	|----|x---|----|x---|
+        (3) hh-open	|--x-|--x-|x-x-|--x-|
+        (5) low-tom	|----|---x|----|----|
+        (12) mid-tom	|----|----|x---|----|
+        (9) hi-tom	|----|----|-x--|----|
+        """,
+      },
+      {"pattern_4.splice",
+        """
+        Saved with HW Version: 0.909
+        Tempo: 240.0
+        (0) SubKick	|----|----|----|----|
+        (1) Kick	|x---|----|x---|----|
+        (99) Maracas	|x-x-|x-x-|x-x-|x-x-|
+        (255) Low Conga	|----|x---|----|x---|
+        """,
+      },
+      {"pattern_5.splice",
+        """
+        Saved with HW Version: 0.708-alpha
+        Tempo: 999.0
+        (1) Kick	|x---|----|x---|----|
+        (2) HiHat	|x-x-|x-x-|x-x-|x-x-|
+        """,
+      },
+    ]
 
     for { filename, output } <- patterns do
       assert decode_file("test/fixtures/#{filename}") == output
